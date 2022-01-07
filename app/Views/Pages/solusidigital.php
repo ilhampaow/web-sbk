d0146aacaf75333971aa5689e6dc638459ccc300
Ilhampaow_309 <ilhampaow@gmail.com>
2022-01-07 21:54:30 +0700

    merapihkan dashboard

--- app/Views/Pages/solusidigital.php
+++ app/Views/Pages/solusidigital.php
@@ -1,15 +1,6 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
-<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url('Assets/images/bg-digital.jpg') ?>');" data-stellar-background-ratio="0.5">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row no-gutters slider-text align-items-end">
-            <div class="col-md-9 ftco-animate pb-5">
-                <p class="breadcrumbs mb-2"><span class="mr-2"><a href="<?= base_url('/'); ?>">USSI-SBK<i class="ion-ios-arrow-forward"></i></a></span></p>
-                <h1 class="mb-0 bread">Solusi Digital</h1>
-            </div>
-        </div>
-    </div>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url('Assets/images/frame-digital1.jpg') ?>');" data-stellar-background-ratio="0.5">
 </section>
 <section class="container mt-3 mb-5">
     <div class="row">
@@ -23,54 +14,23 @@
 </section>
 <section class="services ">
     <div class="container">
-        <div class="card ">
-            <?php $jml_digital = count($digital) ?>
-            <?php for ($i = 0; 0 < $jml_digital;) { ?>
-                <div class="row mr-4 ml-4 mb-5">
-                    <div class="d-flex justify-content-center  ">
-                        <?php if ($i < $jml_digital) { ?>
-                            <div class="col-md-4 mt-5">
-                                <div class="service-item my-auto card">
-                                    <img class="mx-auto d-flex" src="/Assets/images/icon/<?= $digital[$i]['gambar']; ?>" alt="">
-                                    <div class="down-content text-center ">
-                                        <h4><?= $digital[$i]['nama_digital']; ?></h4>
-                                        <a href="/Solusidigital/<?= $digital[$i++]['slug']; ?>" class="filled-button">Read More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php } else {
-                            echo ("oke");
-                            break;
-                        } ?>
-                        <?php if ($i < $jml_digital) { ?>
-                            <div class="col-md-4 mt-5">
-                                <div class="service-item my-auto card">
-                                    <img class="mx-auto d-flex" src="/Assets/images/icon/<?= $digital[$i]['gambar']; ?>" alt="">
-                                    <div class="down-content text-center ">
-                                        <h4><?= $digital[$i]['nama_digital']; ?></h4>
-                                        <a href="/Solusidigital/<?= $digital[$i++]['slug']; ?>" class="filled-button">Read More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php } else {
-                            break;
-                        } ?>
-                        <?php if ($i < $jml_digital) { ?>
-                            <div class="col-md-4 mt-5">
-                                <div class="service-item my-auto card">
-                                    <img class="mx-auto d-flex" src="/Assets/images/icon/<?= $digital[$i]['gambar']; ?>" alt="">
-                                    <div class="down-content text-center ">
-                                        <h4><?= $digital[$i]['nama_digital']; ?></h4>
-                                        <a href="/Solusidigital/<?= $digital[$i++]['slug']; ?>" class="filled-button">Read More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php } else {
-                            break;
-                        } ?>
+        <div class="card">
+            <?php foreach ($digital as $digital) : ?>
+                <div class="row mt-3 mr-2 mb-3 ml-2">
+                    <div class="col-md-4">
+                        <img class="mx-auto w-100" src="/Assets/images/icon/<?= $digital['gambar']; ?>" alt="">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="col-md-12">
+                            <h4><b><?= $digital['nama_digital']; ?></b></h4>
+                            <p style="overflow: hidden; display: -webkit-box; -webkit-line-clamp: 5; -webkit-box-orient: vertical;"><?= $digital['deskripsi']; ?></p>
+                        </div>
+                        <div class="col-md-12 mt-3">
+                            <a href="/Solusidigital/<?= $digital['slug']; ?>" class="filled-button">Read More</a>
+                        </div>
                     </div>
                 </div>
-            <?php } ?>
+            <?php endforeach; ?>
         </div>
 
     </div>
--- app/Views/solusidigital/details.php
+++ app/Views/solusidigital/details.php
@@ -1,15 +1,6 @@
 <?= $this->extend('layout/template'); ?>
 <?= $this->section('content'); ?>
-<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url('Assets/images/bg-digital.jpg') ?>');" data-stellar-background-ratio="0.5">
-    <div class="overlay"></div>
-    <div class="container">
-        <div class="row no-gutters slider-text align-items-end">
-            <div class="col-md-9 ftco-animate pb-5">
-                <p class="breadcrumbs mb-2"><span class="mr-2"><a href="<?= base_url('/'); ?>">USSI-SBK<i class="ion-ios-arrow-forward"></i></a></span></p>
-                <h1 class="mb-0 bread">Solusi Digital</h1>
-            </div>
-        </div>
-    </div>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url('Assets/images/bg-core.jpg') ?>');">
 </section>
 <section class="services">
     <div class="container">
@@ -28,90 +19,58 @@
         </div>
 
         <nav class="nav nav-pills nav-fill d-flex justify-content-center my-auto ">
+            <?php $a = 0; ?>
             <?php foreach ($digital_produk as $digital) :  ?>
-                <button class="col-md-4 card nav-link tablinks ambilDataIdDigital" onclick="openCity(event, '<?= $digital['id_digital_produk']; ?>')">
-                    <a class="my-auto mx-auto"><b><?= $digital['digital_produk']; ?></b></a>
-                </button>
-            <?php endforeach; ?>
+                <?php if ($a == 0) { ?>
+                    <button class="col-md-4 card nav-link tablinks ambilDataIdDigital active" onclick="openCity(event, '<?= $digital['id_digital_produk']; ?>')">
+                    <?php } else { ?>
+                        <button class="col-md-4 card nav-link tablinks ambilDataIdDigital" onclick="openCity(event, '<?= $digital['id_digital_produk']; ?>')">
+                        <?php } ?>
+                        <a class="my-auto mx-auto"><b><?= $digital['digital_produk']; ?></b></a>
+                        </button>
+                        <?php $a++; ?>
+                    <?php endforeach; ?>
 
         </nav>
-
+        <?php $a = 0; ?>
         <?php foreach ($digital_produk as $digital) : ?>
-            <div id="<?= $digital['id_digital_produk']; ?>" class="tabcontent card">
-                <!-- judul -->
-                <div class="row justify-content-center text-center">
-                    <div class="col-md-4 justify-content-center">
-                        <h3><b><?= $digital['digital_produk']; ?></b></h3>
-                    </div>
-                </div>
-                <!-- slider -->
-                <div class="d-flex justify-content-center">
-                    <div class="col-md-2">
-                        <a class="carousel-control" href="#<?= $digital['id_digital_produk']; ?>-<?= $digital['slug']; ?>" data-slide="prev" style="left: 50%;">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                    </div>
-                    <div id="<?= $digital['id_digital_produk']; ?>-<?= $digital['slug']; ?>" class="carousel slide justify-content-center col-md-8" data-ride="carousel">
-                        <!-- Indicators -->
-                        <?php $a = 0; ?>
-                        <ol class="carousel-indicators">
-                            <?php foreach ($digital_produk_dok as $produk_dok) : ?>
-                                <?php if ($produk_dok['source_id_digital_produk'] == $digital['id_digital_produk']) { ?>
-                                    <?php if ($a == 0) { ?>
-                                        <li data-target="#<?= $digital['id_digital_produk']; ?>-<?= $digital['slug']; ?>" data-slide-to="<?= $produk_dok['id_digital_produk_dok']; ?>" class="active"></li>
-                                        <?php $a++; ?>
-                                    <?php } else { ?>
-                                        <li data-target="#<?= $digital['id_digital_produk']; ?>-<?= $digital['slug']; ?>" data-slide-to="<?= $produk_dok['id_digital_produk_dok']; ?>"></li>
-                                    <?php } ?>
-                                <?php } else if ($produk_dok['source_id_digital_produk'] > $digital['id_digital_produk']) {
-                                    break;
-                                } else {
-                                } ?>
-                            <?php endforeach; ?>
-                        </ol>
-                        <!-- Wrapper for slides -->
-                        <?php $b = 0; ?>
-                        <div class="carousel-inner">
-                            <?php foreach ($digital_produk_dok as $produk_dok) : ?>
-                                <?php if ($produk_dok['source_id_digital_produk'] == $digital['id_digital_produk']) { ?>
-                                    <?php if ($b == 0) { ?>
-                                        <div class="item active">
-                                            <img src="/Assets/images/<?= $produk_dok['gambar_produk_dok']; ?>" alt="<?= $produk_dok['gambar_produk_dok']; ?>" style="width:100%;">
-                                        </div>
-                                        <?php $b++; ?>
-                                    <?php } else { ?>
-                                        <div class="item">
-                                            <img src="/Assets/images/<?= $produk_dok['gambar_produk_dok']; ?>" alt="<?= $produk_dok['gambar_produk_dok']; ?>" style="width:100%;">
-                                        </div>
-                                    <?php } ?>
-                                <?php } else if ($produk_dok['source_id_digital_produk'] > $digital['id_digital_produk']) {
-                                    break;
-                                } else {
-                                } ?>
-                            <?php endforeach; ?>
+            <?php if ($a == 0) { ?>
+                <div id="<?= $digital['id_digital_produk']; ?>" class="tabcontent card" style="display: block;">
+                <?php } else { ?>
+                    <div id="<?= $digital['id_digital_produk']; ?>" class="tabcontent card">
+                    <?php } ?>
+                    <?php $a++; ?>
+                    <!-- judul -->
+                    <div class="row justify-content-center text-center">
+                        <div class="col-md-4 justify-content-center">
+                            <h3><b><?= $digital['digital_produk']; ?></b></h3>
                         </div>
-                        <!-- Left and right controls -->
+                    </div>
+                    <!-- artikel -->
+                    <?php foreach ($digital_produk_dok as $produk_dok) : ?>
+                        <?php if ($produk_dok['source_id_digital_produk'] == $digital['id_digital_produk']) { ?>
 
-                    </div>
-                    <div class="col-md-2">
-                        <a class="carousel-control" href="#<?= $digital['id_digital_produk']; ?>-<?= $digital['slug']; ?>" data-slide="next" style="left: 50%;">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <img src="/Assets/images/<?= $produk_dok['gambar_produk_dok']; ?>" alt="<?= $produk_dok['gambar_produk_dok']; ?>" style="width:100%;">
+                                </div>
+                                <div class="col-md-8">
+                                    <p><?= $digital['artikel']; ?></p>
+                                </div>
+                            </div>
+                        <?php } else if ($produk_dok['source_id_digital_produk'] > $digital['id_digital_produk']) {
+                            break;
+                        } else {
+                        } ?>
+                    <?php endforeach; ?>
                     </div>
                 </div>
-                <br>
-                <!-- artikel -->
-                <div class="row d-flex" style="text-align: justify; padding-left: 20px; padding-right: 20px;">
-                    <p><?= $digital['artikel']; ?></p>
-                </div>
-            </div>
-        <?php endforeach; ?>
-    </div>
+            <?php endforeach; ?>
 </section>
 <section>
     <?= $this->include('layout/produklainnya'); ?>
 </section>
 
 <?= $this->endSection('content'); ?>
+
+<!-- slider -->
